0aa70ac7df874ac29b06ed127316121a3d1a2322
cMartinez13 <camilamartinez8134@gmail.com>
2023-10-18 02:27:55 -0300

    Consultas nuevas

--- consultas.sql
+++ consultas.sql
@@ -36,7 +36,7 @@ WHERE t.resumen LIKE '%misión%';
 -- Generar un informe donde se visualicen las series con al menos 3 temporadas.
 SELECT titulo, temporadas
 FROM titulos
-WHERE id_categoria = (SELECT id FROM idcategorias WHERE nombre = 'Series') 
+WHERE id_categoria = (SELECT id FROM idcategorias WHERE nombre = 'Serie') 
 AND temporadas >= 3;
 
 --Encontrar cuántas películas/series trabajó el actor 'Chris Pratt'
@@ -51,32 +51,55 @@ SELECT
 FROM titulos
 WHERE reparto LIKE '%Actor/Actriz%';
 
---Ver solo la categoría "películas". Mostrar el título en mayúscula, el género (en mayúscula), los tags (separados por coma en la misma columna, usando concat o group_concat), duración y el enlace al trailer.
+--7. Ver solo la categoría "películas". Mostrar el título en mayúscula, el género (en mayúscula), los tags (separados por coma en la misma columna, usando concat o group_concat), duración y el enlace al trailer.
 
-SELECT
-    UPPER(ANY_VALUE(t.titulo)) AS Titulo,
-    UPPER(ANY_VALUE(g.nombre)) AS Genero,
-    GROUP_CONCAT(DISTINCT ta.nombre ORDER BY ta.nombre ASC) AS Tags,
-    MAX(t.duracion) AS Duracion,
-    MAX(t.trailer) AS Enlace_Trailer
-FROM titulos AS t
-JOIN idgenero AS g ON t.id_genero = g.id
-JOIN titulos_tags AS tt ON t.id = tt.idtitulo
-JOIN tags AS ta ON tt.idTag = ta.id
-WHERE t.id_categoria = 1
-GROUP BY t.id
+SELECT UCASE(T.titulo) as Película, UCASE(G.nombre) as Genero , T.resumen, T.temporadas, T.trailer, GROUP_CONCAT(D.nombre) as Tags 
+FROM `titulos` T 
+JOIN `idgenero` G 
+ON T.id_genero = G.id 
+INNER JOIN `titulos_tags` B 
+ON T.id = B.idtitulo 
+INNER JOIN `tags` D 
+ON B.idTag = D.id 
+WHERE id_categoria = (SELECT id FROM idcategorias WHERE nombre = 'Película')
+GROUP BY T.titulo;
 
--- Ver solo la categoría "series". Mostrar el título en mayúscula, el género (en mayúscula), los tags (separados por coma en la misma columna, usando concat o group_concat), la cantidad de temporadas, el enlace al trailer y el resumen.
-SELECT
-    UPPER(ANY_VALUE(t.titulo)) AS Titulo,
-    UPPER(ANY_VALUE(g.nombre)) AS Genero,
-    GROUP_CONCAT(DISTINCT ta.nombre ORDER BY ta.nombre ASC) AS Tags,
-    MAX(t.temporadas) AS Cantidad_Temporadas,
-    ANY_VALUE(t.trailer) AS Enlace_Trailer,
-    ANY_VALUE(t.resumen) AS Resumen
-FROM titulos AS t
-JOIN idgenero AS g ON t.id_genero = g.id
-JOIN titulos_tags AS tt ON t.id = tt.idtitulo
-JOIN tags AS ta ON tt.idTag = ta.id
-WHERE t.id_categoria = 2
-GROUP BY t.id;
+--8.Ver solo la categoría "series". Mostrar el título en mayúscula, el género (en mayúscula), los tags (separados por coma en la misma columna, usando concat o group_concat), la cantidad de temporadas, el enlace al trailer y el resumen.
+SELECT UCASE(T.titulo) as Serie, UCASE(G.nombre) as Genero , T.resumen, T.temporadas, T.trailer, GROUP_CONCAT(D.nombre) as Tags 
+FROM `titulos` T 
+JOIN `idgenero` G 
+ON T.id_genero = G.id 
+INNER JOIN `titulos_tags` B 
+ON T.id = B.idtitulo 
+INNER JOIN `tags` D 
+ON B.idTag = D.id 
+WHERE id_categoria = (SELECT id FROM idcategorias WHERE nombre = 'Serie')
+GROUP BY T.titulo;
+--9.Identificar la película/serie con más actores y la que posee menos actores (indicar la cantidad de actores en ambos casos).
+
+
+--10. Contar la cantidad total de películas registradas.
+SELECT COUNT(*) FROM `titulos` 
+WHERE id_categoria = (SELECT id FROM idcategorias WHERE nombre = 'Película');
+
+--11. Contar la cantidad total de series registradas.
+SELECT COUNT(*) FROM `titulos`
+WHERE id_categoria = (SELECT id FROM idcategorias WHERE nombre = 'Serie');
+
+--12. Obtener una lista de series en orden descendente basado en la cantidad de temporadas.
+
+SELECT titulo, temporadas FROM `titulos`
+WHERE id_categoria = (SELECT id FROM idcategorias WHERE nombre = 'Serie')
+ORDER BY temporadas DESC;
+
+-- 14. Buscar películas por palabra clave. Realizar una consulta que permita a los usuarios buscar películas utilizando palabras clave en el título o la descripción
+ SELECT * FROM `titulos` 
+ WHERE id_categoria = (SELECT id FROM idcategorias WHERE nombre = 'Película')
+ AND titulo LIKE 'Ju%';
+
+
+--13. Agregar el campo "Fecha de lanzamiento" a la tabla de trabajos fílmicos como tipo Date y realizar la actualización con las fechas de películas/series del género "Aventura".
+
+
+
+--15. Sumar la tabla "Ranking" que incluye el ID de la película/serie, calificación y comentarios. Utilizar operaciones SQL como joins, unions, concat, count, group by, etc
